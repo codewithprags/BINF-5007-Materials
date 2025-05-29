4a57bad7350e1ebfbc83b2d85687cd118d22583a
codewithprags <pragati.dwivedi@outlook.com>
2025-05-29 15:31:06 -0400

    imput function fixed

--- Assignment 1/Scripts/data_preprocessor.py
+++ Assignment 1/Scripts/data_preprocessor.py
@@ -1,10 +1,20 @@
 # import all necessary libraries here
 import pandas as pd
-import numpy as np
+import numpy as np 
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
+
+#data exploration
+messy_data = pd.read_csv('D:/VSCode ProjectsRepos/BINF-5007-Materials/Assignment 1/Data/messy_data.csv')
+data_explor = messy_data.copy()
+print(data_explor)
+print(data_explor.head())
+
+data_explor.info()
+print(data_explor.isnull().sum())
+
 
 # 1. Impute Missing Values
 def impute_missing_values(data, strategy='mean'):
@@ -14,112 +24,123 @@ def impute_missing_values(data, strategy='mean'):
     :param strategy: str, imputation method ('mean', 'median', 'mode')
     :return: pandas DataFrame
     """
+    #TODO: Fill missing values based on the specified strategy
+    
     messy_data_impute = data.copy()
     
     for col in messy_data_impute:
         if messy_data_impute[col].isnull().any():
-            if strategy == "mean"
-                pd.api.types.is_numeric_dtype(messy_data_impute[col])
-                value_replace = messy_data_impute[col].mean()
-            else
-                value_replace = messy_data_impute[col].mode()[0]
+            try:
+                if strategy == "mean" :
+                    messy_data_impute[col] = messy_data_impute[col].fillna(messy_data_impute[col].mean())
 
-            elif strategy == "median"
-                if 
-            
-    
-    
-    # TODO: Fill missing values based on the specified strategy
-    pass
+                elif strategy == "median":
+                    messy_data_impute[col] = messy_data_impute[col].fillna(messy_data_impute[col].median())
 
-# 2. Remove Duplicates
-def remove_duplicates(data):
-    """
-    Remove duplicate rows from the dataset.
-    :param data: pandas DataFrame
-    :return: pandas DataFrame
-    """
-    # TODO: Remove duplicate rows
-    pass
+                elif strategy == "mode":
+                    messy_data_impute[col] = messy_data_impute[col].fillna(messy_data_impute[col].mode())[0]
+            except:
+                messy_data_impute[col] = messy_data_impute[col].fillna(messy_data_impute[col].mode())[0]
 
-# 3. Normalize Numerical Data
-def normalize_data(data,method='minmax'):
-    """Apply normalization to numerical features.
-    :param data: pandas DataFrame
-    :param method: str, normalization method ('minmax' (default) or 'standard')
-    """
-    # TODO: Normalize numerical data using Min-Max or Standard scaling
-    pass
+    return messy_data_impute
 
-# 4. Remove Redundant Features   
-def remove_redundant_features(data, threshold=0.9):
-    """Remove redundant or duplicate columns.
-    :param data: pandas DataFrame
-    :param threshold: float, correlation threshold
-    :return: pandas DataFrame
-    """
-    # TODO: Remove redundant features based on the correlation threshold (HINT: you can use the corr() method)
-    pass
+# print("this is the test df for imput")
+# test_df = impute_missing_values(messy_data,"mean")
+# test_df.info()
+# print(test_df)
+# print(test_df.isnull().sum())
 
-# ---------------------------------------------------
 
-def simple_model(input_data, split_data=True, scale_data=False, print_report=False):
-    """
-    A simple logistic regression model for target classification.
-    Parameters:
-    input_data (pd.DataFrame): The input data containing features and the target variable 'target' (assume 'target' is the first column).
-    split_data (bool): Whether to split the data into training and testing sets. Default is True.
-    scale_data (bool): Whether to scale the features using StandardScaler. Default is False.
-    print_report (bool): Whether to print the classification report. Default is False.
-    Returns:
-    None
-    The function performs the following steps:
-    1. Removes columns with missing data.
-    2. Splits the input data into features and target.
-    3. Encodes categorical features using one-hot encoding.
-    4. Splits the data into training and testing sets (if split_data is True).
-    5. Scales the features using StandardScaler (if scale_data is True).
-    6. Instantiates and fits a logistic regression model.
-    7. Makes predictions on the test set.
-    8. Evaluates the model using accuracy score and classification report.
-    9. Prints the accuracy and classification report (if print_report is True).
-    """
 
-    # if there's any missing data, remove the columns
-    input_data.dropna(inplace=True)
 
-    # split the data into features and target
-    target = input_data.copy()[input_data.columns[0]]
-    features = input_data.copy()[input_data.columns[1:]]
+# # 2. Remove Duplicates
+# def remove_duplicates(data):
+#     """
+#     Remove duplicate rows from the dataset.
+#     :param data: pandas DataFrame
+#     :return: pandas DataFrame
+#     """
+#     # TODO: Remove duplicate rows
+#     pass
 
-    # if the column is not numeric, encode it (one-hot)
-    for col in features.columns:
-        if features[col].dtype == 'object':
-            features = pd.concat([features, pd.get_dummies(features[col], prefix=col)], axis=1)
-            features.drop(col, axis=1, inplace=True)
+# # 3. Normalize Numerical Data
+# def normalize_data(data,method='minmax'):
+#     """Apply normalization to numerical features.
+#     :param data: pandas DataFrame
+#     :param method: str, normalization method ('minmax' (default) or 'standard')
+#     """
+#     # TODO: Normalize numerical data using Min-Max or Standard scaling
+#     pass
 
-    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, stratify=target, random_state=42)
+# # 4. Remove Redundant Features   
+# def remove_redundant_features(data, threshold=0.9):
+#     """Remove redundant or duplicate columns.
+#     :param data: pandas DataFrame
+#     :param threshold: float, correlation threshold
+#     :return: pandas DataFrame
+#     """
+#     # TODO: Remove redundant features based on the correlation threshold (HINT: you can use the corr() method)
+#     pass
 
-    if scale_data:
-        # scale the data
-        X_train = normalize_data(X_train)
-        X_test = normalize_data(X_test)
+# # ---------------------------------------------------
+
+# # def simple_model(input_data, split_data=True, scale_data=False, print_report=False):
+#     """
+#     A simple logistic regression model for target classification.
+#     Parameters:
+#     input_data (pd.DataFrame): The input data containing features and the target variable 'target' (assume 'target' is the first column).
+#     split_data (bool): Whether to split the data into training and testing sets. Default is True.
+#     scale_data (bool): Whether to scale the features using StandardScaler. Default is False.
+#     print_report (bool): Whether to print the classification report. Default is False.
+#     Returns:
+#     None
+#     The function performs the following steps:
+#     1. Removes columns with missing data.
+#     2. Splits the input data into features and target.
+#     3. Encodes categorical features using one-hot encoding.
+#     4. Splits the data into training and testing sets (if split_data is True).
+#     5. Scales the features using StandardScaler (if scale_data is True).
+#     6. Instantiates and fits a logistic regression model.
+#     7. Makes predictions on the test set.
+#     8. Evaluates the model using accuracy score and classification report.
+#     9. Prints the accuracy and classification report (if print_report is True).
+#     """
+
+#     # if there's any missing data, remove the columns
+#     input_data.dropna(inplace=True)
+
+#     # split the data into features and target
+#     target = input_data.copy()[input_data.columns[0]]
+#     features = input_data.copy()[input_data.columns[1:]]
+
+#     # if the column is not numeric, encode it (one-hot)
+#     for col in features.columns:
+#         if features[col].dtype == 'object':
+#             features = pd.concat([features, pd.get_dummies(features[col], prefix=col)], axis=1)
+#             features.drop(col, axis=1, inplace=True)
+
+#     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, stratify=target, random_state=42)
+
+#     if scale_data:
+#         # scale the data
+#         X_train = normalize_data(X_train)
+#         X_test = normalize_data(X_test)
         
-    # instantiate and fit the model
-    log_reg = LogisticRegression(random_state=42, max_iter=100, solver='liblinear', penalty='l2', C=1.0)
-    log_reg.fit(X_train, y_train)
+#     # instantiate and fit the model
+#     log_reg = LogisticRegression(random_state=42, max_iter=100, solver='liblinear', penalty='l2', C=1.0)
+#     log_reg.fit(X_train, y_train)
 
-    # make predictions and evaluate the model
-    y_pred = log_reg.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    report = classification_report(y_test, y_pred)
+#     # make predictions and evaluate the model
+#     y_pred = log_reg.predict(X_test)
+#     accuracy = accuracy_score(y_test, y_pred)
+#     report = classification_report(y_test, y_pred)
 
-    print(f'Accuracy: {accuracy}')
+#     print(f'Accuracy: {accuracy}')
     
-    # if specified, print the classification report
-    if print_report:
-        print('Classification Report:')
-        print(report)
-        print('Read more about the classification report: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.classification_report.html and https://www.nb-data.com/p/breaking-down-the-classification')
+#     # if specified, print the classification report
+#     if print_report:
+#         print('Classification Report:')
+#         print(report)
+#         print('Read more about the classification report: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.classification_report.html and https://www.nb-data.com/p/breaking-down-the-classification')
     
-    return None
+#     return None
